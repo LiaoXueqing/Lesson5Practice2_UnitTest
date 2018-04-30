360517054883a68c42dbf7ed592b6e939cfd8e5a
xuexue <1047504737@qq.com>
2018-04-30 10:23:30 +0800

    complement

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -14,9 +14,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static tw.core.GameStatus.SUCCESS;
 
 /**
