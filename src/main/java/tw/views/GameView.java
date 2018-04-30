c853071c7b7b96a083816f904015e1a4085290d4
xuexue <1047504737@qq.com>
2018-04-30 10:45:42 +0800

    change println -> print + \n

--- src/main/java/tw/views/GameView.java
+++ src/main/java/tw/views/GameView.java
@@ -19,24 +19,24 @@ public class GameView {
     }
 
     public void showGuessResult(GuessResult guessResult) {
-        System.out.print("Guess Result: " + guessResult.getResult());
+        System.out.print("Guess Result: " + guessResult.getResult()+"\n");
     }
 
     public void showGameStatus(String status) {
-        System.out.print("Game Status: " + status);
+        System.out.print("Game Status: " + status+"\n");
     }
 
     public void showGuessHistory(List<GuessResult> guessResults) {
-        System.out.print("Guess History:");
+        System.out.print("Guess History:\n");
         guessResults.stream().forEach(guessResult -> {
             System.out.print(String.format("[Guess Numbers: %1$s, Guess Result: %2$s]",
                     guessResult.getInputAnswer().toString(),
-                    guessResult.getResult()));
+                    guessResult.getResult())+"\n");
         });
     }
 
     public void showBegin() throws IOException {
-        System.out.print("------Guess Number Game, You have 6 chances to guess!  ------");
+        System.out.print("------Guess Number Game, You have 6 chances to guess!  ------\n");
 
     }
 }
