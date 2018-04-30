c0522535ce62b3c6c5fa192d8e80594095f35929
xuexue <1047504737@qq.com>
2018-04-30 10:11:15 +0800

    complete AnswerGeneratorTest and GameControllerTest

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,67 @@
 package tw.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.commands.InputCommand;
+import tw.core.Answer;
+import tw.core.Game;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.views.GameView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static tw.core.GameStatus.SUCCESS;
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
+    private Game game;
+    private Game gameSpy;
+    private GameController gameController;
+    private InputCommand command;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
+    @Before
+    public void setup() throws OutOfRangeAnswerException {
+        System.setOut(new PrintStream(outContent));
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        game = new Game(answerGenerator);
+        gameSpy = spy(game);
+        command = mock(InputCommand.class);
+    }
+
+    @Test
+    public void should_print_game_success_or_fail_information() throws IOException {
+        when(gameSpy.checkCoutinue()).thenReturn(false);
+        when(gameSpy.checkStatus()).thenReturn(SUCCESS);
+        when(command.input()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        gameController = new GameController(gameSpy,new GameView());
+        gameController.play(command);
+        String result = "Game Status: success\r\n";
+        assertEquals(systemOut(),result);
+    }
+    @Test
+    public void should_print_game_information() throws IOException {
+        when(gameSpy.checkCoutinue()).thenReturn(true).thenReturn(false);
+        when(command.input()).thenReturn(Answer.createAnswer("2 4 7 8"));
+
+        gameController = new GameController(gameSpy,new GameView());
+        gameController.play(command);
+        String result ="Guess Result: 0A2B\r\n"+"Guess History:\r\n"+
+                "[Guess Numbers: 2 4 7 8, Guess Result: 0A2B]\r\n"+
+                "Game Status: continue\r\n" ;
+        assertEquals(systemOut(),result);
+    }
+    private String systemOut() {
+        return outContent.toString();
+    }
 }
--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,30 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static java.lang.Integer.parseInt;
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+
+    @Test
+    public void should_return_whether_generate_answer_correct() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Answer answer = answerGenerator.generate();
+
+        long validateNum = answer.getNumList().stream()
+                .map(num -> parseInt(num))
+                .distinct()
+                .filter(num -> num<10)
+                .count();
+        assertEquals(validateNum,4);
+    }
 
 }
 
